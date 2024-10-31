d71c7a416955981ddb1169ea541a72f52fac12cd
thejourneyofbabo <=>
2024-10-31 10:04:44 +0900

    initial commit with all projects

---
+++ hands_on_data_structures_algorithms/src/bin/simple_rust.rs
@@ -0,0 +1,23 @@
+pub struct Person {
+    name: String,
+    age: i32,
+    children: i32,
+}
+
+impl Person {
+    pub fn print(self) -> String {
+        format!(
+            "name  = {}, age = {} has {} children",
+            self.name, self.age, self.children
+        )
+    }
+}
+
+fn main() {
+    let p = Person {
+        name: "matt".to_string(),
+        age: 35,
+        children: 4,
+    };
+    println!("Hello, people, from {}", p.print());
+}
