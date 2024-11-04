3eddc90a1728e2755a6aadb3577bffc33caea80d
thejourneyofbabo <=>
2024-11-04 20:37:24 +0900

    borrow checker

--- hands_on_data_structures_algorithms/src/bin/pointers-lifetime.rs
+++ hands_on_data_structures_algorithms/src/bin/pointers-lifetime.rs
@@ -9,9 +9,34 @@ impl Person {
         Person { name, age }
     }
 
-    pub fn print(&self) {}
+    pub fn greet(&self) -> String {
+        // self, &self, &mut self, mut self
+        format!("Hi my name is {}", self.name)
+    }
+
+    pub fn age_up(&mut self, n: i32) {
+        self.age += n
+    }
+
+    pub fn dropme(self) {}
 }
 
 fn main() {
-    println!("Hello,");
+    let mut p = Person::new("matt".to_string(), 35);
+    p.age_up(3);
+    let s = p.greet();
+    println!("{}", s);
+
+    let a = get_age(&p);
+    println!("person's age is {}", a);
+    p.age_up(2);
+
+    //p.dropme();
+
+    let s2 = p.greet();
+    println!("really: {}", s2);
+}
+
+pub fn get_age(s: &Person) -> &i32 {
+    &s.age
 }
