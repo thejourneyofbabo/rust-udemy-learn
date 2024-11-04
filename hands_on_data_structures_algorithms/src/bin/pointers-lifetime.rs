3f371e4deda818a341b32e5613cad8bba00439d6
thejourneyofbabo <=>
2024-11-04 16:04:51 +0900

    Pointers-lifetime

---
+++ hands_on_data_structures_algorithms/src/bin/pointers-lifetime.rs
@@ -0,0 +1,17 @@
+#[derive(Debug, Clone)]
+pub struct Person {
+    name: String,
+    age: i32,
+}
+
+impl Person {
+    pub fn new(name: String, age: i32) -> Self {
+        Person { name, age }
+    }
+
+    pub fn print(&self) {}
+}
+
+fn main() {
+    println!("Hello,");
+}
