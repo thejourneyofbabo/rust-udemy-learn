cec0f1da9e059fe7f115b13719e1928201f0894e
thejourneyofbabo <=>
2024-11-08 22:11:14 +0900

    str and String

--- hands_on_data_structures_algorithms/src/bin/ownmemory-heap-box.rs
+++ hands_on_data_structures_algorithms/src/bin/ownmemory-heap-box.rs
@@ -35,4 +35,36 @@ fn main() {
     println!("v.len = {}, capacity = {}", v.len(), v.capacity());
 
     println!("Hello, {:?}", ll);
+
+    let mut s = "   hello   ".to_string();
+
+    let p = s.trim();
+    let p = p.to_string();
+
+    s.push_str("goodbye");
+
+    println!("p == '{}'", p);
+
+    let fstr = "help me find home";
+    let ffstr = string_find_f(fstr);
+    println!("ffstr = '{}'", ffstr);
+    println!("chosen = '{}'", chosose_str(1));
+}
+
+fn string_find_f<'a>(s: &'a str) -> &'a str {
+    //let n = 0;
+    for (n, x) in s.char_indices() {
+        if x == 'f' {
+            return &s[n..];
+        }
+    }
+    s
+}
+
+fn chosose_str(n: i32) -> &'static str {
+    match n {
+        0 => "hello",
+        1 => "goodbye",
+        _ => "other",
+    }
 }
