42b6081daaa54930bdc78acdb06e2deae83c7f71
thejourneyofbabo <journeyofbabo@gmail.com>
2024-11-09 15:49:47 +0900

    bubble sort

--- hands_on_data_structures_algorithms/v1-sorting/src/lib.rs
+++ hands_on_data_structures_algorithms/v1-sorting/src/lib.rs
@@ -1,9 +1,19 @@
-pub fn bubble_sort<T: PartialOrd>(v: &mut [T]) {
-    for _ in 0..v.len() {
-        for i in 0..v.len() - 1 {
+use std::fmt::Debug;
+
+// O(n^2)
+pub fn bubble_sort<T: PartialOrd + Debug>(v: &mut [T]) {
+    for p in 0..v.len() {
+        println!("{:?}", v);
+        let mut sorted = true;
+        for i in 0..(v.len() - 1) - p {
             if v[i] > v[i + 1] {
-                v.swap(i, i + 1)
+                v.swap(i, i + 1);
+                sorted = false;
             }
+        }
+        println!("{:?}", v);
+        if sorted {
+            return;
         }
     }
 }
@@ -14,8 +24,8 @@ mod tests {
 
     #[test]
     fn test_bubble_sort() {
-        let mut v = vec![4, 6, 1, 8, 11, 13];
+        let mut v = vec![4, 6, 1, 8, 11, 13, 3, 2];
         bubble_sort(&mut v);
-        assert_eq!(v, vec![1, 4, 6, 8, 11, 13]);
+        assert_eq!(v, vec![1, 2, 3, 4, 6, 8, 11, 13]);
     }
 }
