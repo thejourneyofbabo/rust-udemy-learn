1a98112c2e0e0ecea74a3fb8c89586895be19223
thejourneyofbabo <journeyofbabo@gmail.com>
2024-11-09 16:16:48 +0900

    Bidirectional bubble sort

--- hands_on_data_structures_algorithms/v1-sorting/src/lib.rs
+++ hands_on_data_structures_algorithms/v1-sorting/src/lib.rs
@@ -11,10 +11,50 @@ pub fn bubble_sort<T: PartialOrd + Debug>(v: &mut [T]) {
                 sorted = false;
             }
         }
-        println!("{:?}", v);
+        //println!("{:?}", v);
         if sorted {
             return;
         }
+    }
+}
+
+pub fn bubble_sort_fb<T: PartialOrd + Debug>(v: &mut [T]) {
+    // Bubble sort forward & backward
+    let len = v.len();
+    if len <= 1 {
+        return;
+    }
+
+    let mut left = 0;
+    let mut right = len - 1;
+    let mut swapped = true;
+
+    while swapped {
+        swapped = false;
+
+        println!("{:?}", v);
+        // Forward pass
+        for i in left..right {
+            if v[i] > v[i + 1] {
+                v.swap(i, i + 1);
+                swapped = true;
+            }
+        }
+        if !swapped {
+            break;
+        }
+
+        right -= 1;
+        swapped = false;
+
+        // Backward pass
+        for i in (left..right).rev() {
+            if v[i] > v[i + 1] {
+                v.swap(i, i + 1);
+                swapped = true;
+            }
+        }
+        left += 1;
     }
 }
 
@@ -26,6 +66,10 @@ mod tests {
     fn test_bubble_sort() {
         let mut v = vec![4, 6, 1, 8, 11, 13, 3, 2];
         bubble_sort(&mut v);
+        println!("\n");
+        let mut v2 = vec![4, 6, 1, 8, 11, 13, 3, 2];
+        bubble_sort_fb(&mut v2);
         assert_eq!(v, vec![1, 2, 3, 4, 6, 8, 11, 13]);
+        assert_eq!(v2, vec![1, 2, 3, 4, 6, 8, 11, 13]);
     }
 }
