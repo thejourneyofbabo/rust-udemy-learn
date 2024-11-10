b6fb743eb9b8fc36f2bf59e2a11b7f9ee7827dec
thejourneyofbabo <journeyofbabo@gmail.com>
2024-11-10 16:16:34 +0900

    mergesort

--- hands_on_data_structures_algorithms/v1-sorting/src/lib.rs
+++ hands_on_data_structures_algorithms/v1-sorting/src/lib.rs
@@ -58,9 +58,66 @@ pub fn bubble_sort_fb<T: PartialOrd + Debug>(v: &mut [T]) {
     }
 }
 
+pub fn merge_sort<T: PartialOrd + Debug>(mut v: Vec<T>) -> Vec<T> {
+    // sort the left half,
+    // sort the right half, O(n*ln(n)) = O(n+2*(n/2)+4*n(4)..)
+    // 1024 * 10 < 1024 * 1024
+    // bring the sorted halfs together O(n)
+
+    if v.len() <= 1 {
+        return v;
+    }
+
+    let mut res = Vec::with_capacity(v.len());
+    let b = v.split_off(v.len() / 2);
+    let a = merge_sort(v);
+    let b = merge_sort(b);
+
+    // Bring them together again add whichever is lowest the front of a or the front of b
+    let mut a_it = a.into_iter();
+    let mut b_it = b.into_iter();
+    let mut a_peek = a_it.next();
+    let mut b_peek = b_it.next();
+
+    loop {
+        match a_peek {
+            Some(ref a_val) => match b_peek {
+                Some(ref b_val) => {
+                    if b_val < a_val {
+                        res.push(b_peek.take().unwrap());
+                        b_peek = b_it.next();
+                    } else {
+                        res.push(a_peek.take().unwrap());
+                        a_peek = a_it.next();
+                    }
+                }
+                None => {
+                    res.push(a_peek.take().unwrap());
+                    res.extend(a_it);
+                    return res;
+                }
+            },
+            None => {
+                if let Some(b_val) = b_peek {
+                    res.push(b_val);
+                }
+                res.extend(b_it);
+                return res;
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_merge_sort() {
+        let v = vec![4, 6, 1, 8, 11, 13, 3, 2];
+        let v = merge_sort(v);
+        assert_eq!(v, vec![1, 2, 3, 4, 6, 8, 11, 13]);
+    }
 
     #[test]
     fn test_bubble_sort() {
